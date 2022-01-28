eb48f75c7649dfd1c64a276efab6b92d4aa135fd
Jade <jadecdrb@gmail.com>
2022-01-28 12:39:42 -0500

    some bookcase inputs added

--- frontend/src/components/NewBookcase.js
+++ frontend/src/components/NewBookcase.js
@@ -1,24 +1,59 @@
-import { useContext } from 'react'
-import { Context } from '../context'
-
+import { useContext, useState } from "react";
+import { Context } from "../context";
 
 const NewBookcase = () => {
+  let { rooms, current } = useContext(Context);
+  let [location, setLocation] = useState("Bookcase Location");
+  let [shelves, setShelves] = useState("");
+  let [width, setWidth] = useState(100);
 
-    let { rooms, current } = useContext(Context)
+  let currentRoom, currentBookcase;
 
-    let currentRoom, currentBookcase;
+  if (current && current.rm) {
+    currentRoom = rooms[rooms.findIndex((r) => r.id === current.rm)];
+    currentBookcase =
+      currentRoom.bookcases[
+        currentRoom.bookcases.findIndex((r) => r.id === current.bc)
+      ];
+  }
 
-    if (current && current.rm) {
-        currentRoom = rooms[rooms.findIndex(r => r.id === current.rm)]
-        currentBookcase = currentRoom.bookcases[currentRoom.bookcases.findIndex(r => r.id === current.bc)]
-    }
+  const handleSubmit = e => {
+      console.log('submit')
+  }
 
+  if (currentRoom && currentBookcase) {
     return (
-        <div>
-            Room ID: {currentRoom?.id},
-            Bookcase ID: {currentBookcase?.id} 
+      <div>
+        <h4>
+            <span style={{ color: 'blue' }}>{location}</span> in {currentRoom.roomName}</h4>
+        <h5>
+          Room ID: {currentRoom?.id}, Bookcase ID: {currentBookcase?.id}
+        </h5>
+        <div className="bookcase" style={{ width: `${width}%` }}>
+            bookcase here
         </div>
-    )
-}
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="bookcase location"
+            value={location}
+          />
+          <input
+            onChange={(e) => setShelves(e.target.value)}
+            placeholder="number of shelves"
+            value={shelves}
+          />
+          <input
+            onChange={(e) => setWidth(e.target.value)}
+            placeholder="number of shelves"
+            type="range"
+            value={width}
+          />
+          <button>Save</button>
+        </form>
+      </div>
+    );
+  } else return <div>No bookcase selected</div>;
+};
 
-export default NewBookcase
+export default NewBookcase;
