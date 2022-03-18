44c8f4a87d28b396481c42fafc32435b53e7ba15
Jade <jadecdrb@gmail.com>
2022-03-18 16:30:53 -0400

    whoops again...

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -9,7 +9,7 @@ class UserService {
     }
 
     awaken() {
-        return axios.post(APILite + `/auth/awaken`).then(r => { alert('Server response: ' + r) })
+        return axios.get(APILite + `/auth/awaken`).then(r => { alert('Server response: ' + r) })
     }
 
     validateUserToken() {
