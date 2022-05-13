94100e4b5d1d20fca11d62c12c8b6906acf59792
Jade <jadecdrb@gmail.com>
2022-05-13 19:37:32 -0400

    added timeout to circumvent state sillyness

--- frontend/src/components/Rooms.js
+++ frontend/src/components/Rooms.js
@@ -323,7 +323,7 @@ const Rooms = () => {
                     {results.length && <option value="results">Search Results:</option>}
                 </select>
                 <input 
-                    onBlur={() => setCategoryDetails(null)}
+                    onBlur={() => setTimeout(() => setCategoryDetails(null), 100)}
                     value={search} 
                     placeholder={searchType}
                     onChange={handleSearchChange}
