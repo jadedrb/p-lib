006e0705af67fe2691b1d9206437b5a2f011afbd
Jade <jadecdrb@gmail.com>
2022-07-03 15:09:11 -0400

    yet another attempt at home screen icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -80,7 +80,7 @@ function App() {
       console.time('time')
       validate()
       mounted.current = true
-      console.log('v1.53')
+      console.log('v1.54')
 
       setTimeout(() => {
         if (document.querySelector('.rooms'))
