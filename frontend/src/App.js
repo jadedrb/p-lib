fa0e29edd062c57dccf9eb31dd0bee75a98d125f
Jade <jadecdrb@gmail.com>
2022-07-10 12:23:59 -0400

    adjusted some css and made bookcases a set width on small screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -80,7 +80,7 @@ function App() {
       console.time('time')
       validate()
       mounted.current = true
-      console.log('v1.61')
+      console.log('v1.62')
 
       setTimeout(() => {
         if (document.querySelector('.rooms'))
