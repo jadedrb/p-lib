708bff90797343ce808f10f8657c40f42dc4005d
Jade <jadecdrb@gmail.com>
2023-09-27 17:52:40 -0400

    whoops with the password

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,10 @@
-const Pool = require('pg').Pool
-
 require('dotenv').config()
+
+const Pool = require('pg').Pool
 
 const pool = new Pool({
     user: process.env.DB_USERNAME,
-    password: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
